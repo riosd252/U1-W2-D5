8b34885ee510dfb33dac92877eac922d3263cdef
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2023-12-31 18:12:08 +0100

    Completed.

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -25,7 +25,8 @@ for (let i = 0; i < pets.length; i++) {
 console.log("Esercizio 2");
 
 {
-  const alphaPets = pets.sort();
+  const petsCopy = pets.slice(0);
+  const alphaPets = petsCopy.sort();
 
   console.log(alphaPets);
 }
@@ -37,7 +38,7 @@ console.log("Esercizio 2");
 console.log("Esercizio 3");
 
 {
-  const pets1 = pets.slice();
+  const pets1 = pets.slice(0);
 
   reversePets = pets1.reverse();
 
@@ -105,6 +106,7 @@ const newCar = {
   model: "Italia",
   color: "red",
   trims: ["corsa", "dolce", "boh"],
+  licensePlate: "XZ08DR",
 };
 
 cars.push(newCar);
@@ -166,9 +168,103 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
 
+let i = 0;
+
+while (numericArray[i] !== 66) {
+  console.log(numericArray[i]);
+  i++;
+}
+
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+const alphabetArray = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      alphabetArray.push(1);
+      break;
+    case "b":
+      alphabetArray.push(2);
+      break;
+    case "c":
+      alphabetArray.push(3);
+      break;
+    case "d":
+      alphabetArray.push(4);
+      break;
+    case "e":
+      alphabetArray.push(5);
+      break;
+    case "f":
+      alphabetArray.push(6);
+      break;
+    case "g":
+      alphabetArray.push(7);
+      break;
+    case "h":
+      alphabetArray.push(8);
+      break;
+    case "i":
+      alphabetArray.push(9);
+      break;
+    case "j":
+      alphabetArray.push(10);
+      break;
+    case "k":
+      alphabetArray.push(11);
+      break;
+    case "l":
+      alphabetArray.push(12);
+      break;
+    case "m":
+      alphabetArray.push(13);
+      break;
+    case "n":
+      alphabetArray.push(14);
+      break;
+    case "o":
+      alphabetArray.push(15);
+      break;
+    case "p":
+      alphabetArray.push(16);
+      break;
+    case "q":
+      alphabetArray.push(17);
+      break;
+    case "r":
+      alphabetArray.push(18);
+      break;
+    case "s":
+      alphabetArray.push(19);
+      break;
+    case "t":
+      alphabetArray.push(20);
+      break;
+    case "u":
+      alphabetArray.push(21);
+      break;
+    case "v":
+      alphabetArray.push(22);
+      break;
+    case "w":
+      alphabetArray.push(23);
+      break;
+    case "x":
+      alphabetArray.push(24);
+      break;
+    case "y":
+      alphabetArray.push(25);
+      break;
+    case "z":
+      alphabetArray.push(26);
+      break;
+  }
+}
+
+console.log(alphabetArray);
